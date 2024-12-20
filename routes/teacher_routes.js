c30d534ca220be1c25e4abc007eddd4394e9bb80
Ismael110599 <156389046+Ismael110599@users.noreply.github.com>
2024-12-20 08:59:51 -0500

    Semana 8

--- routes/teacher_routes.js
+++ routes/teacher_routes.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
+const teacherController = require("../controllers/teacher.controller")
+
 /* GET home page. */
-router.get('/list', function(request, response,) {
+router.get('/list', function(response) {
+   const result =  teacherController.getList()
   response.status(200).json({
     status:true,
     message: "Todo esta ok",
